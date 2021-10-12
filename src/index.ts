c34da21f100f31d373e70903ff9a99d2de539aab
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berkbuyukdurak07@gmail.com>
2021-10-13 01:13:28 +0300

    Middlewares, exceptions and customer repository is added.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { createConnection } from "typeorm";
 import express, { Application } from "express";
 import morgan from "morgan";
 import Router from "./routes/routes";
-import dbConfig from "./config/db"
+import dbConfig from "./config/db";
+import errorMiddleware from "./middlewares/error.middleware";
 
 const PORT = process.env.PORT || 8000;
 
@@ -12,13 +13,9 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
+app.use(errorMiddleware);
 
 app.use(Router);
-/*
-app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
-});
-*/
 
 createConnection(dbConfig)
   .then((_connection) => {
